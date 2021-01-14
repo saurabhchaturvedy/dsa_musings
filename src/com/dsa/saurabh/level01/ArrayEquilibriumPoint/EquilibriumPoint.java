a1d30846128275720e086097a180ad0f5eced1d9
Saurabh Chaturvedi <saurabh.chaturvedi@mendix.com>
2021-01-14 13:45:22 +0530

    understanding equilibrium point problem

---
+++ src/com/dsa/saurabh/level01/ArrayEquilibriumPoint/EquilibriumPoint.java
@@ -0,0 +1,31 @@
+package com.dsa.saurabh.level01.ArrayEquilibriumPoint;
+
+public class EquilibriumPoint {
+
+
+    private static int equilibriumPoint(int arr[]) {
+        int rightSum = 0;
+        int leftSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            rightSum = rightSum + arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            rightSum = rightSum - arr[i];
+            {
+                if (leftSum == rightSum) {
+                    return i;
+                }
+                leftSum = leftSum + arr[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 2, 2};
+        System.out.println(equilibriumPoint(arr));
+    }
+}
