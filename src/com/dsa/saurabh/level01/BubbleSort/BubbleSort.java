af58ddaad81f9f9b8cc31d1121a4b3bed38757e6
Saurabh Chaturvedi <saurabh.chaturvedi@mendix.com>
2021-01-15 23:04:28 +0530

    understanding bubble sort approach

---
+++ src/com/dsa/saurabh/level01/BubbleSort/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.dsa.saurabh.level01.BubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+
+    private static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {9, 6, 5, 0, 8, 2};
+        Arrays.stream(bubbleSort(arr)).asLongStream().forEach(System.out::println);
+    }
+}
