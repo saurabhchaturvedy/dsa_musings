cfc4adc65617a08c57d4676986d854a995276d6e
Saurabh Chaturvedi <saurabh.chaturvedi@mendix.com>
2021-01-29 22:15:52 +0530

    understanding trie data structure

--- src/com/dsa/saurabh/level01/Tries/Node.java
+++ src/com/dsa/saurabh/level01/Tries/Node.java
@@ -12,8 +12,40 @@ public class Node {
         this.children = new Node[Constant.ALPHABET_SIZE];
     }
 
-    private void setChild(int index, Node node) {
+    public void setChild(int index, Node node) {
         this.children[index] = node;
+    }
+
+    public Node getChild(int index) {
+        return this.children[index];
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public Node[] getChildren() {
+        return children;
+    }
+
+    public boolean isLeaf() {
+        return leaf;
+    }
+
+    public void setLeaf(boolean leaf) {
+        this.leaf = leaf;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
     }
 
     @Override
--- src/com/dsa/saurabh/level01/Tries/Trie.java
+++ src/com/dsa/saurabh/level01/Tries/Trie.java
@@ -2,13 +2,29 @@ package com.dsa.saurabh.level01.Tries;
 
 public class Trie {
 
-    Node trie;
+    Node root;
 
     Trie() {
-        this.trie = new Node("");
+        this.root = new Node("");
     }
 
     public void insert(String word) {
+
+        Node tempNode = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if (tempNode.getChild(index) == null) {
+                Node node = new Node(String.valueOf(c));
+                tempNode.setChild(index, node);
+                tempNode = node;
+            } else {
+                tempNode = tempNode.getChild(index);
+            }
+        }
+
+        tempNode.setLeaf(true);
 
     }
 
